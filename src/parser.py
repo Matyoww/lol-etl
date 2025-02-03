9a40e8a7ed153229963fc3f6c1fd4ceee39fae9c
Matthew Concepcion <matthewconcepcion10@gmail.com>
2025-02-04 01:35:25 +0800

    fix: set exact version for python

---
+++ src/parser.py
@@ -0,0 +1,20 @@
+# TODO:
+
+# Prerequisites
+# - Data model
+
+# Extract process
+# 1. Extract data into a dataframe
+# 2. Clean the data
+
+# Transform process
+# 3. Group all relevant data from the dataframe
+# 4. 
+
+# Load process
+# 5. Load the data into the database
+
+
+def get_match_metadata():
+    # this includes the time, game mode, map
+    pass
