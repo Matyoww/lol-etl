7ff092e798412b032460f12a6ddc55610a76fa56
Matthew Concepcion <matthewconcepcion10@gmail.com>
2025-07-26 01:12:30 +0800

    chore: remove unnecessary function

--- src/utils.py
+++ src/utils.py
@@ -8,14 +8,6 @@ def send_get_request(url):
     data = response.json()
     return data
 
-def get_champion_mapping(champ_id_list):
-    with open('./metadata/champion.json') as f:
-        champ_data = json.load(f)
-
-    champ_mapping = {int(champ_data['data'][key]['key']):champ_data['data'][key]['name'] for key in champ_data['data'].keys()}
-    champ_list = {champ_mapping[key] for key in champ_id_list}
-    return champ_list
-
 def extract_player_match_data(match_data, game_name, tagline):
     game_info = match_data['info']
     players_game_info = game_info['participants']
