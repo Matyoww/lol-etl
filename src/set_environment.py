1663da245e876559f85dad6d5bc8063dd67d2152
Matthew Concepcion <matthewconcepcion10@gmail.com>
2025-08-09 01:03:12 +0800

    chore: set .env file manually

--- src/set_environment.py
+++ src/set_environment.py
@@ -8,6 +8,15 @@ default_values = {
 }
 
 def set_environment():
+    env_file = './.env'
+    if os.path.exists(env_file):
+        with open(env_file, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    key, value = line.split('=', 1)
+                    os.environ[key.strip()] = value.strip()
+
     for key in default_values.keys():
         if key not in os.environ:
             os.environ[key] = default_values[key]
